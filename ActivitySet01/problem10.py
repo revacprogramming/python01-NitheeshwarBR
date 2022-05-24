4c977f3daeb73103643d12d6c4bef803beffe327
NitheeshwarBR <nitheeshwar.r.g@gmail.com>
2022-05-24 21:56:58 +0530

    p10.py(24/5/2022)

--- ActivitySet01/problem10.py
+++ ActivitySet01/problem10.py
@@ -1,6 +1,30 @@
-# Files
-# 7.2 Write a program that prompts for a file name, then opens that file and reads through the file, looking for lines of the form:
-# X-DSPAM-Confidence:    0.8475
-# Count these lines and extract the floating point values from each of the lines and
-#  compute the average of those values and produce an output as shown below. 
-# Do not use the sum() function or a variable named sum in your solution.
+#dictionaries
+# 9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. 
+# The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. 
+# The program creates a Python dictionary that maps the sender's 
+# mail address to a count of the number of times they appear in the file. After the dictionary is produced, 
+# the program reads through the dictionary using a maximum 
+# loop to find the most prolific committer.
+lst=[]
+mails={}
+file=open(input())
+for i in file:
+    if i.startswith('From:'):
+        continue
+    elif i.startswith('From'):
+        words=i.split()
+        word=lst.append(words[1])
+for i in lst:
+    if i not in mails:
+        mails[i]=1
+    else:
+        mails[i]=mails[i]+1
+max=0   
+for i in mails:
+    if mails[i]>max:
+        max=mails[i]
+        largest=i
+    elif mails[i]<max:
+        continue
+        
+print(largest,mails[largest])
---
+++ ActivitySet03/problem01.py
@@ -0,0 +1,34 @@
+# Problem 1: Rectangle Area
+# Develop a program that, given as input the cartesian coordinates of three vertices of a Rectangle,
+# reports the area of that Rectangle. You will recall that the area of a Rectangle is the product of
+# the lengths of any two adjacent sides.
+# Input: The first line contains a positive integer n indicating how many Rectangles are to be
+# analyzed. Each Rectangle is described on a single line via six real numbers, x1, y1, x2, y2, x3,
+# and y3, separated by spaces. These provide the coordinates of three of the Rectangle’s vertices,
+# namely P1(x1, y1), P2(x2, y2), and P3(x3, y3).
+# Output: For each Rectangle provided as input, report its area.
+# Sample input
+# ------------
+# 3
+# 0.0 0.0 0.0 1.0 1.0 0.0
+# -1.0 2.0 3.0 5.0 1.0 1.0
+# 5.0 9.0 -0.5 0.0 7.5 5.0
+# Resultant output
+# ----------------
+# Area of Rectangle with vertices (0.0,0.0),(0.0,1.0),(1.0,0.0) is 1.0
+# Area of Rectangle with vertices (-1.0,2.0),(3.0,5.0),(1.0,1.0) is 10.0
+# Area of Rectangle with vertices (5.0,9.0),(-0.5,0.0),(7.5,5.0) is 44.5
+from math import *
+n=int(input("Enter limit:"))
+for i in range(0,n+1):
+    if i<n:
+        x1,y1,x2,y2,x3,y3=input().split()
+        (X1,Y1,X2,Y2,X3,Y3)=(float(x1),float(y1),float(x2),float(y2),float(x3),float(y3))
+        area_Rectangle=2*(0.5*fabs((((X1*(Y2-Y3))+(X2*(Y3-Y1))+(X3*(Y1-Y2))))))
+        print("Area of Rectangle with vertices (%0.1f,%0.1f),(%0.1f,%0.1f),(%0.1f,%0.1f) is %0.1f\n"%(X1,Y1,X2,Y2,X3,Y3,area_Rectangle))
+    else:
+        break
+
+    
+
+
